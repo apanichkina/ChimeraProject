5638d17352ed040e5bc8333f1ace264f1bab665b
iHelos <ihelos.ermakov@gmail.com>
2016-03-27 23:40:14 +0300

    VK SDK added

--- app/src/main/java/duality/chimera/MainActivity.java
+++ app/src/main/java/duality/chimera/MainActivity.java
@@ -2,12 +2,22 @@ package duality.chimera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.util.VKUtil;
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        for (int i = 0; i < fingerprints.length; i++)
+        {
+            Log.v(TAG, fingerprints[i]);
+        }
+
     }
 }
---
+++ app/src/main/java/duality/socialnetwork/mutations/VKSdkMutant.java
@@ -0,0 +1,15 @@
+package duality.socialnetwork.mutations;
+
+import android.support.annotation.Nullable;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKSdk;
+
+import java.util.Map;
+
+/**
+ * Created by iHelo on 28.03.2016.
+ */
+public class VKSdkMutant {
+    
+}
